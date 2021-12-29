b2c20e4d5cba7da4a28f59a6cfeafa60291f5751
guna_9 <guna_9@naver.com>
2021-12-30 01:04:20 +0900

    header css, scroll js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,25 @@ $(document).ready(function(){
     $('#header').load("header.html");
     $('#footer').load("footer.html");
 });
+
+
+
+//mobile-scroll-header.fixed
+const header = document.getElementById('header');
+const mobileFooterTab = document.getElementById('mobile-footer-tab');
+let lastScrollTop = 0;
+
+function scrollFunc() { 
+    let st = window.pageXOffset || document.documentElement.scrollTop;
+
+    if (st > lastScrollTop && st >= 120) {
+        header.classList.add('scroll-down');
+        mobileFooterTab.classList.add('scroll-down');
+    }else{
+        header.classList.remove('scroll-down');
+        mobileFooterTab.classList.remove('scroll-down');
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+}
+
+window.addEventListener('scroll', scrollFunc);
